312089751403ca0e5db1d45a3c414d4ad956ac54
Roopashree <roopashree.lakshmikeshava@tarento.com>
2023-08-28 15:28:50 +0530

    added initial project structure, add common shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'src/material/material.module';
-import { HttpClientModule } from '@angular/common/http';
-import { AuthModulesModule } from './modules/auth-modules/auth-modules.module';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { SharedModule } from './shared';
-import { CoreModule } from './core/core.module';
-import { GrievanceModulesModule } from './modules/grievance-modules/grievance-modules.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserProfileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    HttpClientModule,
-    AuthModulesModule,
-    SharedModule,
-    ReactiveFormsModule,
-    CoreModule,
-    GrievanceModulesModule,
-    ToastrModule.forRoot()
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/guards/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./auth-guard/auth.guard";
-export * from "./role-content-guard/role-content.guard";
