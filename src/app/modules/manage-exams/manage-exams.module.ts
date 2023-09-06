a10cdc79deeb6853c5996b3ef07578465b8dc607
mansurskTarento <mansur.shaik@tarento.com>
2023-09-06 17:00:00 +0530

    adding modules

---
+++ src/app/modules/manage-exams/manage-exams.module.ts
@@ -0,0 +1,23 @@
+//#region (imports)
+
+//#region ()
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//#endregion
+
+//#region (created modules && components)
+import { ManageExamsRoutingModule } from './manage-exams-routing.module';
+//#endregion
+
+//#endregion
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManageExamsRoutingModule
+  ]
+})
+export class ManageExamsModule { }
---
+++ src/app/modules/mange-question-papers/mange-question-papers.module.ts
@@ -0,0 +1,23 @@
+//#region (imports)
+
+//#region ()
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//#endregion
+
+//#region (created modules && components)
+import { MangeQuestionPapersRoutingModule } from './mange-question-papers-routing.module';
+//#endregion
+
+//#endregion
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MangeQuestionPapersRoutingModule
+  ]
+})
+export class MangeQuestionPapersModule { }
