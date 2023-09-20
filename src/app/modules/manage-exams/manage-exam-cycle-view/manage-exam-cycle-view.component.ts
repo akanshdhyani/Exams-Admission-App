6a8b84653713cfe8e022af932ef637242982d678
devendra <devendra@devendras-MacBook-Pro.local>
2023-09-20 18:55:14 +0530

    Feat: View pre created exam cycle

--- src/app/modules/manage-exams/manage-exam-cycle-view/manage-exam-cycle-view.component.ts
+++ src/app/modules/manage-exams/manage-exam-cycle-view/manage-exam-cycle-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-exam-cycle-view',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-exam-cycle-view.component.scss']
 })
 export class ManageExamCycleViewComponent {
+
+  constructor(private router: Router) {
+    
+  }
+
+  onCancel() {
+    this.router.navigateByUrl('/manage-exam-cycle')
+  }
 
 }
